96efa079f4909d67bfd26e95ef601ba1b1dc2585
kiwilamb <kiwilamb@googlemail.com>
2017-01-10 19:25:02 +0000

    facialrecog: adding ability to detect largest face in frame, slows from 15 FPS to 4 FPS

--- facialrecog.py
+++ facialrecog.py
@@ -1,47 +1,52 @@
 import os
 import cv2
 import time
-import dlib
+from openface import align_dlib
 
 
 class FacialRecog:
 
     def __init__(self, file=0, timeout=10):
-        self.file = file
-        self.face_detector = dlib.get_frontal_face_detector()
+        self.device = "webcam" if file == 0 else file
+        self.face_alinger = align_dlib.AlignDlib("shape_predictor_68_face_landmarks.dat")
         # If the file is 0 use the webcam as the video capture device
-        if self.file is not 0:
-            if not os.path.isfile(self.file):
-                raise IOError("Couldn't find file {}".format(self.file))
+        if file is not 0:
+            if not os.path.isfile(file):
+                raise IOError("Couldn't find file {}".format(file))
 
-        self.video = cv2.VideoCapture(self.file)
+        self.video = cv2.VideoCapture(file)
         timeout += time.time()
         while not self.video.isOpened() and time < timeout:
-            self.video = cv2.VideoCapture(self.file)
+            self.video = cv2.VideoCapture(file)
             cv2.waitKey(1000)
             print("Waiting for the video capture to start...")
 
         if not self.video.isOpened():
-            raise Exception("Couldn't connect to {}".format("webcam" if self.file == 0 else self.file))
+            raise Exception("Couldn't connect to {}".format(self.device))
 
-    def play_video(self):
-        '''
+    def process_frames(self):
+        """
         Plays video feed (webcam or video file)
         :return: nothing
-        '''
-        print("Playing {} video ...".format("webcam" if self.file == 0 else self.file))
+        """
+        print("Playing {} video ...".format(self.device))
         start = time.time()
         frames = 0
         while True:
             frames += 1
             flag, frame = self.video.read()
+            largest_face_box = self.face_alinger.getLargestFaceBoundingBox(frame)
+            if largest_face_box is not None:
+                top_left = (largest_face_box.left(), largest_face_box.top())
+                bottom_right = (largest_face_box.right(), largest_face_box.bottom())
+                cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0))
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) == 27:
-                print("Stopping {} video".format("webcam" if self.file == 0 else self.file))
+                print("Stopping {} video".format(self.device))
                 break
-            if self.file is not 0 and self.video.get(cv2.CAP_PROP_POS_FRAMES) == self.video.get(cv2.CAP_PROP_FRAME_COUNT):
+            if self.device is not "webcam" and self.video.get(cv2.CAP_PROP_POS_FRAMES) == self.video.get(cv2.CAP_PROP_FRAME_COUNT):
                 # This works as it checks if it is a file first, if not it doesn't bother with other checks
-                print("Stopping {} video".format("webcam" if self.file == 0 else self.file))
+                print("Stopping {} video".format(self.device))
                 break
         fps = frames / (time.time() - start)
         print("FPS: {}".format(fps))
@@ -49,4 +54,4 @@ class FacialRecog:
 
 if __name__ == '__main__':
     fr = FacialRecog()
-    fr.play_video()
+    fr.process_frames()
